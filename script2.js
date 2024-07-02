72de6a12bd72ee95705df5fbf5b05f55013b566e
burgerralf <167563043+burgerralf@users.noreply.github.com>
2024-07-02 13:52:11 +0200

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,13 +1,5 @@
 // script.js
 
-document.getElementById('chat-icon').addEventListener('click', function() {
-  var chatWindow = document.getElementById('chat-window');
-  if (chatWindow.style.display === 'none' || chatWindow.style.display === '') {
-    chatWindow.style.display = 'block';
-  } else {
-    chatWindow.style.display = 'none';
-  }
-
 function isMobileDevice() {
   return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
 }
